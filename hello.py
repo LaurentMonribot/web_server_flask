269a19b2dd4107b66081f50c8ea34710b8b34d77
Cloud User <centos@laurent.novalocal>
2018-05-11 11:16:49 +0000

    manager.run replaced by app.run

--- hello.py
+++ hello.py
@@ -40,5 +40,7 @@ def index():
     return render_template('index.html', form=form, result=session.get('result'))
 
 
+#if __name__ == '__main__':
+#    manager.run()
 if __name__ == '__main__':
-    manager.run()
+    app.run(host='0.0.0.0', debug=True)
